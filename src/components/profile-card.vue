7bc4a6f063c8724e5fdaa080e0692e8ec40ee3cc
Nikita Kozlov <kozlov_nikita@outlook.com>
2022-01-14 20:38:03 +0300

    style (start): profile-card.vue

--- src/components/profile-card.vue
+++ src/components/profile-card.vue
@@ -2,7 +2,12 @@
   <div class="tt-profile">
     <div class="tt-profile__user">
       <div class="tt-profile__avatar">
-        <img class="tt-profile__image" :src="require('@/assets/' + avatar)" alt="User avatar" />
+        <img
+          class="tt-profile__image"
+          draggable="false"
+          :src="require('@/assets/' + avatar)"
+          alt="User avatar"
+        />
       </div>
       <div class="tt-profile__heading">
         <p class="tt-profile__title">Report for</p>
@@ -10,9 +15,18 @@
       </div>
     </div>
     <div class="tt-rofile__timeline">
-      <button class="tt-profile__button">Daily</button>
-      <button class="tt-profile__button active">Weekly</button>
-      <button class="tt-profile__button">Monthly</button>
+      <button class="tt-profile__button" @click="changeTimeline(), changeActiveButton()">
+        Daily
+        <img class="tt-profile__button--icon" src="@/assets/img/arrow-right.png" alt="Arrow icon" />
+      </button>
+      <button class="tt-profile__button active" @click="changeTimeline(), changeActiveButton()">
+        Weekly
+        <img class="tt-profile__button--icon" src="@/assets/img/arrow-right.png" alt="Arrow icon" />
+      </button>
+      <button class="tt-profile__button" @click="changeTimeline(), changeActiveButton()">
+        Monthly
+        <img class="tt-profile__button--icon" src="@/assets/img/arrow-right.png" alt="Arrow icon" />
+      </button>
     </div>
   </div>
 </template>
@@ -30,6 +44,14 @@ export default {
       default: 'img/image-jeremy.png',
     },
   },
+  methods: {
+    changeTimeline() {
+      console.log('Timeline!')
+    },
+    changeActiveButton() {
+      console.log('Button!')
+    },
+  },
 }
 </script>
 
@@ -39,6 +61,89 @@ export default {
   width: 100%;
   background-color: var(--color-blue-dark);
   border-radius: 16px;
+  overflow: hidden;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1.4fr 0.6fr;
+  gap: 0px 0px;
+  grid-template-areas:
+    '.'
+    '.';
+  justify-items: center;
+  align-items: start;
+}
+.tt-profile__user {
+  width: 100%;
+  height: 100%;
+  background-color: var(--color-primary);
+  padding: 48px 24px;
+  border-radius: 16px;
+}
+.tt-profile__avatar {
+  background-color: white;
+  height: 84px;
+  width: 84px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100%;
+}
+.tt-profile__image {
+  width: 80px;
+  height: 80px;
+}
+.tt-profile__heading {
+  width: 100%;
+  margin-top: 32px;
+}
+.tt-profile__title {
+  font-size: 0.8rem;
+  color: var(--color-blue-text);
+}
+.tt-profile__name {
+  color: white;
+  font-size: 2.4rem;
+  font-weight: 300;
+}
+.tt-rofile__timeline {
+  width: 100%;
+  height: 100%;
   padding: 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+}
+.tt-profile__button {
+  font-size: 1rem;
+  color: var(--color-blue-text);
+  cursor: pointer;
+  border-radius: 8px;
+  padding: 4px 8px;
+  margin-left: -8px;
+  transition: all 0.4s cubic-bezier(0.25, 0.8, 0.25, 1);
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.tt-profile__button.active {
+  color: white;
+}
+.tt-profile__button:hover {
+  background-color: var(--color-blue-hover);
+  color: white;
+}
+.tt-profile__button:focus {
+  background-color: var(--color-blue-hover);
+  color: white;
+}
+.tt-profile__button--icon {
+  max-height: 100%;
+  height: 12px;
+  transition: all 0.4s cubic-bezier(0.25, 0.8, 0.25, 1);
+  opacity: 0;
+}
+.tt-profile__button:hover .tt-profile__button--icon {
+  opacity: 1;
 }
 </style>
