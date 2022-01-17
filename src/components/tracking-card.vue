f4c8a2acae35acbd2b00be1c5da592583e70fc99
Nikita Kozlov <kozlov_nikita@outlook.com>
2022-01-18 02:26:55 +0300

    markup: complete tracking-card.vue

--- src/components/tracking-card.vue
+++ src/components/tracking-card.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="tt-card" :style="{backgroundColor: `var(--color-${cardAsset.color})`}"></div>
+  <div class="tt-card" :style="{backgroundColor: `var(--color-${cardAsset.color})`}">
+    <img
+      class="tt-card__icon"
+      :src="require('@/assets/img/' + cardAsset.icon)"
+      alt="Activity Icon"
+    />
+    <div class="tt-card__data">
+      <div class="tt-card__heading">
+        <h2 class="tt-card__title">{{ cardAsset.title }}</h2>
+        <button class="tt-card__button">
+          <svg class="tt-card__button-svg" width="21" height="5" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
+              fill="#BBC0FF"
+              fill-rule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+      <div class="tt-card__tracking">
+        <h3 class="tt-card__current">{{ timeframes.weekly.current }}</h3>
+        <p class="tt-card__previous">Last Week - {{ timeframes.weekly.previous }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -68,11 +92,9 @@ export default {
   height: 100%;
   width: 100%;
   border-radius: 16px;
+  overflow: hidden;
 }
 
 @media screen and (max-width: 769px) {
-  .tt-card {
-    min-height: 256px;
-  }
 }
 </style>
