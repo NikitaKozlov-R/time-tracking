70a57f021ed7556432dc002fbb1b8c57a33ee517
Nikita Kozlov <kozlov_nikita@outlook.com>
2022-01-22 18:13:24 +0300

    refactor: add root path

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,13 @@ import TimeTracking from '@/views/time-tracking.vue'
 
 const routes = [
   {
-    path: '/time-tracking/:timeframe',
+    path: '/time-tracking/',
     name: 'Time Tracking',
+    component: TimeTracking,
+  },
+  {
+    path: '/time-tracking/:timeframe',
+    name: 'Time Tracking Selected',
     component: TimeTracking,
   },
 ]
